405d2cf45b4c5d89f4d5686512736209d99b392b
rainbow <498088467>
2017-08-17 11:43:11 +0800

    2017-8-17 11:42 修改了OaToMes_ZKCG_Action.java中的注释信息

--- src/weaver/interfaces/sap/OaToMes_ZKCG_Action.java
+++ src/weaver/interfaces/sap/OaToMes_ZKCG_Action.java
@@ -26,23 +26,26 @@ public class OaToMes_ZKCG_Action implements Action {
 	public void oaToMes(RequestInfo request) {
 		String requsetId = request.getRequestid();// 获取当前流程的requsetId
 		try {
-			sql = "SELECT a.LOT_CODE,a.MATERIAL FROM FORMTABLE_MAIN_68_DT2 a INNER JOIN FORMTABLE_MAIN_68 b ON b.ID = a.MAINID WHERE b.REQUESTID = '"
-					+ requsetId + "'";
+			sql = "SELECT a.LOT_CODE,a.MATERIAL FROM FORMTABLE_MAIN_68_DT1 a INNER JOIN FORMTABLE_MAIN_68 b ON b.ID = a.MAINID WHERE b.REQUESTID = '"
+					+ requsetId + "'";// 获取当前requestId的明细表1的内容
 			rs.execute(sql);
 			while (rs.next()) {
-				json.put("userId", "OA");
-				json.put("factory", "FGS");
-				json.put("lotId", rs.getString("LOT_CODE"));
-				json.put("matId", rs.getString("MATERIAL"));
+				json.put("userId", "OA");// 用户id 固定写死为OA
+				json.put("factory", "FGS");// 工厂 固定写死为FGS
+				json.put("lotId", rs.getString("LOT_CODE"));// 明细表1生产批号
+				json.put("matId", rs.getString("MATERIAL"));// 明细表1料号
 				json.put("matVer", "1");
 				json.put("delCode", "NOSENDSAP");
 				req.put("message", json);
 				req.put("fromSystem", "TEST");
 				req.put("functionName", "MES_TERMINATELOT");
-				req.put("token", "TOKEN");
+				req.put("token", "TESTTOKEN");
+				String retSrcs = HttpClientJson.readInterfacePost("http://172.16.59.54:8080/MesWebService/req",
+						req.toString());// 向MES传输json对象
+				baseBean.writeLog(retSrcs);// 抓取返回值
 			}
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 	}
 
